0eb9833db12fbf64d241a5d47cfcd0ea23c83923
Alexander Cavalheiro Becker <callfrygou@hotmail.com>
2020-04-06 15:33:04 -0300

    Creates RequestStageResource

--- src/main/java/com/course/springcourse/resource/RequestResource.java
+++ src/main/java/com/course/springcourse/resource/RequestResource.java
@@ -1,7 +1,9 @@
 package com.course.springcourse.resource;
 
 import com.course.springcourse.domain.Request;
+import com.course.springcourse.domain.RequestStage;
 import com.course.springcourse.service.RequestService;
+import com.course.springcourse.service.RequestStageService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -14,6 +16,9 @@ import java.util.List;
 public class RequestResource {
     @Autowired
     private RequestService requestService;
+
+    @Autowired
+    private RequestStageService requestStageService;
 
     @PostMapping
     public ResponseEntity<Request> save(@RequestBody Request request) {
@@ -38,5 +43,11 @@ public class RequestResource {
     public ResponseEntity<List<Request>> listAll() {
         List<Request> requests = requestService.listAll();
         return ResponseEntity.ok(requests);
+    }
+
+    @GetMapping("/{id}/request-stages")
+    public ResponseEntity<List<RequestStage>> listAllStagesById(@PathVariable(name = "id") Long id) {
+        List<RequestStage> stages = requestStageService.listAllByRequestId(id);
+        return ResponseEntity.ok(stages);
     }
 }
